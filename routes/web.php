d171da43a3c5b658bbcf6136e7ae7fdf80845427
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-10-25 23:53:45 +0700

    Create Controller Admin

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function() {
+    Route::resource('users', 'UsersController', ['except' => ['show', 'create', 'store']]);
+});
+
